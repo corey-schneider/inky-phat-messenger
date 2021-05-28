6c15c4030dc41033b7499f576977133c8b967fba
Corey <cschne11@uncc.edu>
2021-05-28 19:28:06 -0400

    added support for multiline message

--- message.py
+++ message.py
@@ -7,6 +7,12 @@ from font_fredoka_one import FredokaOne
 from inky.auto import auto
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--message', '-m', type=str, required=True, help="The message to display on the screen")
+args = parser.parse_args()
+
+message = args.message
+
 # This function will take a quote as a string, a width to fit
 # it into, and a font (one that's been loaded) and then reflow
 # that quote with newlines to fit into the space required.
@@ -51,26 +57,43 @@ max_height = h - padding - font.getsize("ABCD ")[1]
 below_max_length = False
 
 
-def print_text(text):
-    """Displays text in Inky pHAT display.
-    Args:
-        text: String that is displayed on the Inky pHAT
-    """
-    img = Image.new("P", (w, h))
-    draw = ImageDraw.Draw(img)
-    
-    message = text
-    width, height = font.getsize(message)
-    x_axis = (w / 2) - (width / 2)
-    y_axis = (h / 2) - (height / 2)
-    draw.text((x_axis, y_axis), message, inky_display.BLACK, font)
-    inky_display.set_image(img)
-    inky_display.show()
+# Create the canvas to draw on
+img = Image.new("P", (w, h))
+draw = ImageDraw.Draw(img)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--message', '-m', type=str, required=True, help="The message to display on the screen")
-args = parser.parse_args()
 
-message = args.message
+while not below_max_length:
 
-print_text(message)
+    reflowed = reflow_quote(message, max_width, font)
+    p_w, p_h = font.getsize(reflowed)  # Width and height of quote
+    p_h = p_h * (reflowed.count("\n") + 1)   # Multiply through by number of lines
+
+    if p_h < max_height:
+        below_max_length = True              # The quote fits! Break out of the loop.
+
+    else:
+        continue
+
+# x- and y-coordinates for the top left of the quote
+message_x = (w - max_width) / 2
+message_y = ((h - max_height) + (max_height - p_h - font.getsize("ABCD ")[1])) / 2
+
+
+draw.multiline_text((message_x, message_y), reflowed, fill=inky_display.BLACK, font=font, align="left")
+
+print(reflowed + "\n" + message + "\n")
+
+# Display the completed canvas on Inky wHAT
+inky_display.set_image(img)
+inky_display.show()
+
+# def print_text(message):
+#     width, height = font.getsize(message)
+#     x_axis = (w / 2) - (width / 2)
+#     y_axis = (h / 2) - (height / 2)
+#     draw.text((x_axis, y_axis), message, inky_display.BLACK, font)
+#     inky_display.set_image(img)
+#     inky_display.show()
+
+
+# print_text(message)
