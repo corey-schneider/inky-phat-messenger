08adee9ac8e71c7e2896c5fa9a38d345dd032848
Corey <cschne11@uncc.edu>
2021-05-28 12:11:10 -0400

    first test

---
+++ script.py
@@ -0,0 +1,17 @@
+from PIL import Image, ImageFont, ImageDraw
+
+img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
+draw = ImageDraw.Draw(img)
+
+from font_fredoka_one import FredokaOne
+
+font = ImageFont.truetype(FredokaOne, 22)
+
+message = "Hello, World!"
+w, h = font.getsize(message)
+x = (inky_display.WIDTH / 2) - (w / 2)
+y = (inky_display.HEIGHT / 2) - (h / 2)
+
+draw.text((x, y), message, inky_display.RED, font)
+inky_display.set_image(img)
+inky_display.show()
